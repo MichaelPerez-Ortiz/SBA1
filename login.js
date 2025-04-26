4fa4753f574d20a6ee6694f88beff25502be10db
Michael Pérez-Ortiz <perezortizm7@gmail.com>
2025-04-26 12:31:38 -0400

    Changed navbar and dropdown menu / added images / restyled all pages/ working on image carousel

---
+++ login.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    document.getElementById("drop").classList.toggle("show");
+  }
+  
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("drop");
+      if (drop.classList.contains('show')) {
+        drop.classList.remove('show');
+        }
+      }
+    }
+
+
+
+
+
+
+
+
+
